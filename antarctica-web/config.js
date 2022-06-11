55874e98edb6d1ae11c51268dcfbbd7815a7a82c
c1Audio <c@misuco.org>
2022-06-11 23:04:12 +0200

    config for node server

---
+++ antarctica-web/config.js
@@ -0,0 +1,8 @@
+const config = {
+ app: {
+   web_path: '/home/c1/MISUCO/antarctica/antarctica-web/static',
+   bin_path: '/home/c1/MISUCO/antarctica/build-antarctica-midigen-Desktop_Qt_5_15_2_GCC_64bit-Debug/antarctica'
+ }
+};
+
+module.exports = config;
--- antarctica-web/server.js
+++ antarctica-web/server.js
@@ -1,6 +1,8 @@
 const https = require('https')
 const fs = require('fs')
 const express = require('express')
+const config = require('./config')
+
 var proc = require('child_process');
 
 const app = express()
@@ -11,20 +13,20 @@ app.use(express.json()) // for parsing application/json
 
 app.get('/newclip', function(req, res) {
   console.log("got newclip " + req.query.id);
-  proc.execSync('/home/c1/MISUCO/antarctica/build-antarctica-midigen-Desktop_Qt_5_15_2_GCC_64bit-Debug/antarctica -t '+req.query.tempo+' -p ' + req.query.pitch + ' -b '+req.query.clipId+' -l '+req.query.loopLength+' -r '+req.query.repeat+' -n '+req.query.basenote+' -s '+req.query.scale+' -a '+req.query.arrange+' -o /home/c1/MISUCO/antarctica/antarctica-web/static/loops/'+req.query.id);
+  proc.execSync(config.app.bin_path + ' -t '+req.query.tempo+' -p ' + req.query.pitch + ' -b '+req.query.clipId+' -l '+req.query.loopLength+' -r '+req.query.repeat+' -n '+req.query.basenote+' -s '+req.query.scale+' -a '+req.query.arrange+' -o /home/c1/MISUCO/antarctica/antarctica-web/static/loops/'+req.query.id);
   res.send("loops/" + req.query.id +".wav");
 });
 
 app.get('/rate', function(req, res) {
   console.log("got rating " + req.query.rating + " for " + req.query.trackId);
   var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress  
-  proc.execSync('echo $(date +%Y-%m-%d-%H-%M-%S) '+ip+' >> /home/c1/MISUCO/antarctica/antarctica-web/static/'+req.query.trackId+'-r-'+req.query.rating);
+  proc.execSync('echo $(date +%Y-%m-%d-%H-%M-%S) '+ip+' >> '+config.app.web_path+'/'+req.query.trackId+'-r-'+req.query.rating);
   res.send("Thanks for your rating for "+req.query.trackId);
 });
 
 app.get('/files', function(req, res) {
   console.log("got files request for spot " + req.query.spotId);
-  var files = proc.execSync('ls -1 /home/c1/MISUCO/antarctica/antarctica-web/static/loops/*'+req.query.spotId+'*.wav | xargs -n 1 basename');
+  var files = proc.execSync('ls -1 '+config.app.web_path+'/loops/*'+req.query.spotId+'*.wav | xargs -n 1 basename');
   res.send(files);
 });
 
